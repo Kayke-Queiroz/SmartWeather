9b53ded38a00189a829d11b68f0ae9bf8bbec59a
Kayke-Queiroz <kaykekato@gmail.com>
2026-03-25 09:12:50 -0300

    add url vercel

--- back/config/middlewares.ts
+++ back/config/middlewares.ts
@@ -4,7 +4,19 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        /\.vercel\.app$/,         // cobre qualquer preview do Vercel
+        'https://smart-weather-sigma.vercel.app/', // ← troque pela sua URL exata do Vercel
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
